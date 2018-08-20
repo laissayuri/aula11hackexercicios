c9faabd3daf472dfbc4b0042946dca8af9fffc5f
laissayuri <k.lais.sayuri@gmail.com>
2018-08-20 14:49:07 -0300

    Com juros

--- ex1.js
+++ ex1.js
@@ -15,55 +15,73 @@
 let idade;
 let renda;
 let emprestimo;
-let erroidade;
-let errorenda;
-let erroemprestimo;
+let erro_idade;
+let erro_renda;
+let erro_emprestimo;
+let emprestimo_aprovado;
+let num_parcelas;
+let parcela;
+let juros = 0.06;
+let total;
 
 idade = Number(prompt("Quantos anos você tem?"));
 
 if (idade >= 22 && idade <= 60){
     console.log("Idade válida");
-    erroidade = false;
+    erro_idade = false;
 }
 else if (isNaN(Number(idade)) || idade === null ) {
     console.log("Idade inválida");
-    erroidade = true;
+    erro_idade = true;
 } else {
     console.log("Idade Inválida");
-    erroidade = true;
+    erro_idade = true;
 }
 
 renda = Number(prompt("Qual é a sua renda?"));
 
 if (renda >= 1500) {
     console.log("Renda Válida");
-    errorenda = false;
+    erro_renda = false;
 } 
 else if (isNaN(Number(renda)) || idade === null ) {
     console.log("Idade inválida");
-    errorenda = true;
+    erro_renda = true;
 }
 else {
     console.log("Renda Inválida");
-    errorenda = true;
+    erro_renda = true;
 }
 
 emprestimo = Number(prompt("Quanto você quer de empréstimo?"));
 
 if (emprestimo >= 1000 && emprestimo <= renda*20) {
     console.log("Valor Permitido");
-    erroemprestimo = false;
+    erro_emprestimo = false;
 }
 else if (isNaN(Number(emprestimo)) || idade === null ) {
     console.log("Idade inválida");
-    erroemprestimo = true;
+    erro_emprestimo = true;
 } else {
     console.log("Valor Inválido");
-    erroemprestimo = true;
+    erro_emprestimo = true;
 }
 
-if (erroidade == false && errorenda == false && erroemprestimo == false){
-    console.log("Empréstimo Válido");
+if (erro_idade == false && erro_renda == false && erro_emprestimo == false){
+    console.log("Empréstimo Permitido");
+    emprestimo_aprovado = true;
 } else {
-    console.log("Empréstimo Inválido");
+    console.log("Empréstimo Não Permitido");
+    emprestimo_aprovado = false;
 }
+
+if (emprestimo_aprovado == true){
+    num_parcelas = Number(prompt("Em quantas parcelas mensais com juros de 6% a.m. deseja realizar o pagamento? Você pode escolher entre 1 a 36"));
+    if(num_parcelas >= 1 && num_parcelas <= 36){
+        parcela = (emprestimo/num_parcelas)+(emprestimo*juros);
+        total = emprestimo*(1 + (juros*num_parcelas));
+        alert(`Você terá que pagar ${num_parcelas} parcelas de ${parcela}, dando um total de ${total} reais`);
+    }
+}
+
+
